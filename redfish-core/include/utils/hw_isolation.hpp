f3074ef8872aa169532fc60683b59eef2f03efae
Ramesh Iyyar <rameshi1@in.ibm.com>
2022-04-19 08:11:26 -0500

    HW-Isolation: Fix, Don't handle the Unavailable D-Bus error
    - The backend application returns an "Unavailable" error when
      the hardware isolation service is disabled by using
      the "allow_hw_isolation" settings and the bmcweb returning
      "PropertyValueResourceConflict" error.
    
    - But, we don't have any use case for the user to enable/disable
      the hardware isolation service, just required for the lab use case
      so, the redfish server doesn't need to process the "Unavailable"
      error and returns the Redfish error since the user cannot enable
      through the Redfish, and also it is not going to happen in the field
      so removed the "Unavailable" error.
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: I748648365333dd1bf259ab091dacf54ea9705078

--- redfish-core/include/utils/hw_isolation.hpp
+++ redfish-core/include/utils/hw_isolation.hpp
@@ -133,13 +133,6 @@ inline void
         {
             retChassisPowerStateOffRequiredError(aResp, resourceObjPath);
         }
-        else if (strcmp(dbusError->name, "xyz.openbmc_project.Common.Error."
-                                         "Unavailable") == 0)
-        {
-            messages::propertyValueResourceConflict(
-                aResp->res, "Enabled", std::to_string(enabledPropVal ? 1 : 0),
-                "HardwareIsolation");
-        }
         else if (strcmp(dbusError->name, "xyz.openbmc_project."
                                          "HardwareIsolation.Error."
                                          "IsolatedAlready") == 0)
@@ -272,17 +265,6 @@ inline void
                                         "NotAllowed") == 0)
             {
                 retChassisPowerStateOffRequiredError(aResp, resourceObjPath);
-            }
-            else if (strcmp(dbusError->name, "xyz.openbmc_project.Common.Error."
-                                             "Unavailable") == 0)
-            {
-                // The Enabled property value will be "true" to
-                // de-isolate.
-                constexpr bool enabledPropVal = true;
-                messages::propertyValueResourceConflict(
-                    aResp->res, "Enabled",
-                    std::to_string(enabledPropVal ? 1 : 0),
-                    "HardwareIsolation");
             }
             else if (strcmp(dbusError->name, "xyz.openbmc_project.Common.Error."
                                              "InsufficientPermission") == 0)
